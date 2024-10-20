bec37f6807303d220965a6aac46973e42ed57d1c
damir-tahirovic <dtahirovic2@gmail.com>
2024-10-20 15:56:00 +0200

    https://matijabojanic.atlassian.net/browse/OL-32 Fixed Model names and made relations to them from Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -21,24 +21,24 @@ class Book extends Model
         'dimensions',
     ];
 
-//    public function bookAuthors()
-//    {
-//        return $this->hasMany(BookAuthor::class);
-//    }
-//
-//    public function bookPublishers()
-//    {
-//        return $this->hasMany(BookPublisher::class);
-//    }
-//
-//    public function bookGenres()
-//    {
-//        return $this->hasMany(BookGenre::class);
-//    }
-//
-//    public function bookCategories()
-//    {
-//        return $this->hasMany(BookCategory::class);
-//    }
+    public function Authors()
+    {
+        return $this->hasMany(Author::class);
+    }
+
+    public function Publishers()
+    {
+        return $this->hasMany(Publisher::class);
+    }
+
+    public function Genres()
+    {
+        return $this->hasMany(Genre::class);
+    }
+
+    public function Categories()
+    {
+        return $this->hasMany(Category::class);
+    }
 
 }
