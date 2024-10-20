a07b359b35ef8901fc8ead75a49edf1e7ed46c36
damir-tahirovic <dtahirovic2@gmail.com>
2024-10-20 18:37:06 +0200

    https://matijabojanic.atlassian.net/browse/OL-35 Created view methods for books

--- routes/api.php
+++ routes/api.php
@@ -46,10 +46,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/publishers/create', [PublisherController::class, 'createPublisher'])->name('publishers.create');
     Route::get('/auth/publishers/{id}', [PublisherController::class, 'viewPublisher'])->middleware('checkLibrarian')->name('publishers.view');
     Route::delete('/auth/publishers/{id}', [PublisherController::class, 'deletePublisher'])->middleware('checkLibrarian')->name('publishers.delete');
-Route::get('/auth/publishers', [PublisherController::class, 'viewAllPublishers'])->name('publishers.index');
+    Route::get('/auth/publishers', [PublisherController::class, 'viewAllPublishers'])->name('publishers.index');
 
     // Book routes
-    Route::post('/auth/books/create', [BookController::class, 'createBook'])->name('books.create');
+    Route::post('/auth/books/create', [BookController::class, 'createBook'])->middleware('checkLibrarian')->name('books.create');
+    Route::get('/auth/books/{id}', [BookController::class, 'viewBook'])->middleware('checkLibrarian')->name('books.view');
+    Route::get('/auth/books', [BookController::class, 'viewAllBooks'])->middleware('checkLibrarian')->name('books.index');
 });
 
 
