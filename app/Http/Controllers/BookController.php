a02f652f51d75fd8f4a03f303d43d359c87fe134
damir-tahirovic <dtahirovic2@gmail.com>
2024-10-20 18:46:53 +0200

    https://matijabojanic.atlassian.net/browse/OL-36 Made book updating method

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -121,4 +121,83 @@ class BookController extends Controller
 
         return response()->json($book);
     }
+
+    public function updateBook(Request $request, $id)
+    {
+        $requestData = json_decode($request->get('data'), true);
+
+        echo json_encode($requestData, JSON_PRETTY_PRINT);
+
+        $validatedData = validator($requestData, [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'number_of_pages' => 'required|integer',
+            'number_of_copies' => 'required|integer',
+            'isbn' => 'required|string|max:13',
+            'language' => 'required|string|max:255',
+            'script' => 'required|in:' . implode(',', Book::SCRIPTS),
+            'binding' => 'required|in:' . implode(',', Book::BINDINGS),
+            'dimensions' => 'required|in:' . implode(',', Book::DIMENSIONS),
+            'categories' => 'required|array',
+            'genres' => 'required|array',
+            'authors' => 'required|array',
+            'publishers' => 'required|array',
+        ])->validate();
+
+        $book = Book::findOrFail($id);
+
+        $allCategories = Category::pluck('id')->toArray();
+        $allGenres = Genre::pluck('id')->toArray();
+        $allAuthors = Author::pluck('id')->toArray();
+        $allPublishers = Publisher::pluck('id')->toArray();
+
+        if (array_diff($validatedData['categories'], $allCategories)) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        if (array_diff($validatedData['genres'], $allGenres)) {
+            return response()->json(['message' => 'Genre not found'], 404);
+        }
+
+        if (array_diff($validatedData['authors'], $allAuthors)) {
+            return response()->json(['message' => 'Author not found'], 404);
+        }
+
+        if (array_diff($validatedData['publishers'], $allPublishers)) {
+            return response()->json(['message' => 'Publisher not found'], 404);
+        }
+
+        DB::beginTransaction();
+        try {
+            $book->update([
+                'name' => $validatedData['name'],
+                'description' => $validatedData['description'],
+                'number_of_pages' => $validatedData['number_of_pages'],
+                'number_of_copies' => $validatedData['number_of_copies'],
+                'isbn' => $validatedData['isbn'],
+                'language' => $validatedData['language'],
+                'script' => $validatedData['script'],
+                'binding' => $validatedData['binding'],
+                'dimensions' => $validatedData['dimensions'],
+            ]);
+
+            $book->categories()->sync($validatedData['categories']);
+            $book->genres()->sync($validatedData['genres']);
+            $book->authors()->sync($validatedData['authors']);
+            $book->publishers()->sync($validatedData['publishers']);
+
+            if ($request->hasFile('image')) {
+                $book->clearMediaCollection('book_images');
+                $book->addMediaFromRequest('image')
+                    ->toMediaCollection('book_images');
+            }
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+
+        return response()->json($book, 200);
+    }
 }
